bcff1081157df05cd26df66f598e8463b4cb1e18
FranEG80 <fjenriglez@gmail.com>
2021-01-24 16:31:40 +0100

    Feat: Adding languague 'es-ES'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'mi App para aprender Angular';
+
+export class AppComponent implements OnInit {
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  title = 'myFirstApp';
+
+  ngOnInit(): void {
+    this.document.documentElement.lang = 'es-ES';
+  }
+
 }
